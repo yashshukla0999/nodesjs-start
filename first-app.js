4b0c0a2724076531bb557a7bdc9dbd6a22f9b7e5
yashshukla099 <yashshukla0999@gmail.com>
2023-06-13 18:37:12 +0530

    createing servers

--- first-app.js
+++ first-app.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+const http = require('http');
+const server= http.createServer((req,res)=>{
+    console.log("yash shukla");
+
+})
+server.listen(4000);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+setTimeout(() => {console.log('d')}, 0)

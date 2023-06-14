279dfcd732984893359a8d7e5bf97f205bef7928
yashshukla099 <yashshukla0999@gmail.com>
2023-06-14 21:04:29 +0530

    exporting file

--- first-app.js
+++ first-app.js
@@ -1,44 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes=require('./routes');
 
-const server = http.createServer((req,res)=>{
-    const method = req.method;
-    const url = req.url
-        if(url==='/'){
-   
-            fs.readFile("massage.txt",(err,data)=>{
-                if(err){
-                    console.log(err);
-                }
-                            res.write('<html><body><form action="/massage" method="POST"><input type="text"><button type="submit">send</button></form></body>');
-res.write("<html>")
-                res.write(`<body>${data}</body`)
-           
-                res.write('<html><body><form action="/massage" method="POST"><input type="text"><button type="submit">send</button></form></body>');
-                res.write("</html>")
-            return res.end();
-    
-        }) 
-        }
-       else  if(url==='/massage'&& method==='POST'){
-        res.on("data",(chunk)=>{
-            body.push(chunk);
 
-        });
-        return req.on("end",()=>{
-            const parsedBody =buffer.concat(body).toString();
-            const massage =parsedBody.split("=")[1];
-            fs.writeFileSync("massage.txt",massage,(err)=>{
-                res.statusCode=302;
-                res.setHeader("Location","/");
-                return res.end();
-            });
-           
-        });
-           
-        }
-       
-       
-
-});
+const server = http.createServer(routes)
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const requestHandler =(req,res)=>{
+    const method = req.method;
+    const url = req.url
+    
+if (url === '/') {
+
+    fs.readFile("massage.txt", (data, err) => {
+        if (err) {
+            console.log(err);
+        }
+        res.write('<html><body><form action="/massage" method="POST"><input type="text"><button type="submit">send</button></form></body>');
+       
+        res.write(`<body>${data}</body`)
+
+   
+        return res.end();
+
+    })
+}
+else if (url === '/massage' && method === 'POST') {
+    const body=[];
+    req.on("data", (chunk) => {
+        body.push(chunk);
+
+    });
+    return req.on("end", () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const massage = parsedBody.split("=")[1];
+        fs.writeFileSync("massage.txt", massage, (err) => {
+            
+        });
+        res.statusCode = 302;
+            res.setHeader("Location", "/");
+            return res.end();
+
+    });
+
+}
+
+
+};
+module.exports=requestHandler;
+
